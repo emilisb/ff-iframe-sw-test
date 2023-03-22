162fd844d9e351590e27f530f7926fe6ec712d51
Emilis Baliukonis <emilisb@wix.com>
2023-03-22 15:56:48 +0200

    Update worker log messages

--- cross-origin/worker.js
+++ cross-origin/worker.js
@@ -2,13 +2,17 @@ function makeRequest() {
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "./intercept", true);
   xhr.onload = function () {
-    console.log("Response: " + xhr.responseText);
     if (xhr.responseText === "intercepted") {
       console.log("[SUCCESS]: TEST PASSED");
+    } else {
+      console.error(
+        '[ERROR]: TEST FAILED: expected "intercepted", got ' +
+          xhr.responseText.slice(0, 30)
+      );
     }
   };
   xhr.onerror = function () {
-    console.error("Error" + xhr.status);
+    console.error("Request failed with status " + xhr.status);
     console.error("[ERROR]: TEST FAILED");
   };
   xhr.send();
